dcff14090920664c6087665590a1000e45a36443
Palina Supranovich <palina.supranovich@mend.io>
2026-04-02 11:38:50 +0200

    Add azure package Go modules project

---
+++ go.mod
@@ -0,0 +1,7 @@
+module data
+
+go 1.16
+
+require (
+	github.com/Azure/go-autorest v14.2.0+incompatible
+)
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	_ "github.com/Azure/go-autorest"
+)
+
+func main() {
+	fmt.Println("Hello")
+}
